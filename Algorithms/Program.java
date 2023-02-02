191d648fa395fb75ff36912d8f8e6e5c2c52aaa0
Pragati Gaje <pdgaje19@gmail.com>
2023-02-02 15:05:33 +0530

    second commit

--- Algorithms/Program.java
+++ Algorithms/Program.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+
 public class Program {
+    
     public static void LinearSearch(int[]arr, int key)
     {
         boolean flag = false;
@@ -14,10 +17,41 @@ public class Program {
         if(flag == false)
             System.out.println("Key not found");
     }
+
+    public static void BinarySearch(int[] arr , int key)
+    {
+        int count =0;
+        int index=0;
+        Arrays.sort(arr);
+        int first = 0, last = arr.length-1;
+        int mid;
+
+        while(first<=last)
+        {
+            mid=(first+last)/2;
+
+            if(arr[mid]==key)
+            {
+                count++;
+                index=mid;
+            }
+            else if(key> arr[mid])
+            {
+                first=mid+1;
+            }
+            else if(key< arr[mid])
+            {
+                last=mid-1;
+            }
+        }
+        if(count!=0)
+            System.out.println("Key Found at "+index);
+    }
     public static void main(String[] args) {
 
-        int[] arr = {10,42,84,47,65};
+        int[] arr = {10,42,84,14,85,74,98,56,74,47,65};
 
-        LinearSearch(arr, 84);
+        //LinearSearch(arr, 84);
+        BinarySearch(arr, 74);
     }
 }
